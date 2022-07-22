dc2d5e3df113cb402b46d40eaa54bcd77422ee36
Павел Пацей <patsey.pavel@gmail.com>
2022-07-22 12:46:33 +0300

    add users app and create follow model

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "djoser",
     "api",
+    "users",
 ]
 
 MIDDLEWARE = [
---
+++ backend/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Подписчик",
+        related_name="follower",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="На кого подписались",
+        related_name="following",
+    )
+
+    def __str__(self):
+        return f"{self.user}_to_{self.author}"
