0b34a029eaa16a7624c76e5910459370150f0d94
Павел Пацей <patsey.pavel@gmail.com>
2022-07-29 00:16:44 +0300

    pip install load_dotenv and add SECRET_KEY in .env

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,15 +1,21 @@
 import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(BASE_DIR / "../.env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1r068&5fjv4fzg36572op9pd1bzt*axiv48njz&kyo=wt2d90e"
+# SECRET_KEY = "django-insecure-1r068&5fjv4fzg36572op9pd1bzt*axiv48njz&kyo=wt2d90e"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
