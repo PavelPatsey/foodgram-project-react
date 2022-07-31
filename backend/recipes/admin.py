b9c4b79547238f3b6822cfa1704a5859cae5652a
Павел Пацей <patsey.pavel@gmail.com>
2022-07-31 19:09:02 +0300

    add Recipe in admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredient, Tag
+from .models import Ingredient, Recipe, Tag
 
 
 @admin.register(Tag)
@@ -19,4 +19,18 @@ class IngredientAdmin(admin.ModelAdmin):
         "pk",
         "name",
         "measurement_unit",
+    )
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "tags",
+        "author",
+        "ingredients",
+        "name",
+        "image",
+        "text",
+        "cooking_time",
     )
