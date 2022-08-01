a67399061a522a6b0ef0ff2f81cdcd9c48a609f5
Павел Пацей <patsey.pavel@gmail.com>
2022-08-01 18:30:39 +0300

    заменил модель Follow на Subscription

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,19 +4,25 @@ from django.db import models
 User = get_user_model()
 
 
-class Follow(models.Model):
+class Subscription(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name="Подписчик",
-        related_name="follower",
+        related_name="subscribers",
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name="На кого подписались",
-        related_name="following",
+        verbose_name="Автор на которого подписались",
+        related_name="subscribed_authors",
     )
+
+    class Meta:
+        unique_together = (
+            "user",
+            "author",
+        )
 
     def __str__(self):
         return f"{self.user}_to_{self.author}"
