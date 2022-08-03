a02a73a9ae7fab2cebb5409f47cc2597f0c981dd
Павел Пацей <patsey.pavel@gmail.com>
2022-08-03 15:52:01 +0300

    поправил поля в IngredientAmountSerializer

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -29,9 +29,11 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class IngredientAmountSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source="ingredient.id")
-    name = serializers.ReadOnlyField(source="ingredient.name")
-    measurement_unit = serializers.ReadOnlyField(
+    id = serializers.PrimaryKeyRelatedField(
+        queryset=Ingredient.objects.all(),
+    )
+    name = serializers.CharField(source="ingredient.name")
+    measurement_unit = serializers.CharField(
         source="ingredient.measurement_unit",
     )
 
