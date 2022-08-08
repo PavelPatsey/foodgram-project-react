65b14eca4191f12716d347699c02a2ff1c6cf99a
Павел Пацей <patsey.pavel@gmail.com>
2022-08-08 18:39:09 +0300

    git add test_delete_recipe_to_favorites_authorized_client

--- backend/recipes/tests/test_recipes.py
+++ backend/recipes/tests/test_recipes.py
@@ -1094,3 +1094,11 @@ class RecipeTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         test_json = {"errors": "Рецепт уже добавлен в избранное"}
         self.assertEqual(response.json(), test_json)
+
+    def test_delete_recipe_to_favorites_authorized_client(self):
+        """Удалить рецепт из избранного авторизованным пользователем."""
+        url = f"/api/recipes/{self.recipe.id}/favorite/"
+        response = self.authorized_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.authorized_client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
