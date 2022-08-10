2a61b33645c0ce5a1348c7b17f58c2685a25b712
Павел Пацей <patsey.pavel@gmail.com>
2022-08-10 12:57:34 +0300

    renamed:    users/tests/test_subscription.py -> users/tests/test_models.py

--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -1,5 +1,3 @@
-import unittest
-
 from django.db import IntegrityError
 from django.test import TestCase
 from rest_framework import status
@@ -17,7 +15,7 @@ class UsersViewsTest(TestCase):
         cls.authorized_client.force_authenticate(cls.user)
 
     def test_cool_test(self):
-        """cool test"""
+        """Cool test."""
         self.assertEqual(True, True)
 
     def test_no_self_subscription(self):
@@ -26,3 +24,15 @@ class UsersViewsTest(TestCase):
         constraint_name = "prevent_self_subscription"
         with self.assertRaisesMessage(IntegrityError, constraint_name):
             Subscription.objects.create(user=user, author=user)
+
+    def test_add_recipe_to_shopping_cart_authorized_client(self):
+        """Подписаться авторизованным пользователем."""
+        count = Subscription.objects.count()
+        test_user = User.objects.create_user(username="test_username")
+        url = f"/api/users/{test_user.id}/subscribe/"
+        breakpoint()
+        response = self.authorized_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Subscription.objects.count(), count + 1)
+        test_json = {}
+        self.assertEqual(response.json(), test_json)
