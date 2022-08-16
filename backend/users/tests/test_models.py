be01d3d8a21d818d86892871c245a33b7d79fb28
Павел Пацей <patsey.pavel@gmail.com>
2022-08-16 16:48:25 +0300

    поправил тесты в test_models.py

--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -7,24 +7,31 @@ from ..models import Subscription, User
 class UsersViewsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.user = User.objects.create(username="user")
+        cls.author = User.objects.create(username="author")
 
     def test_cool_test(self):
         """Cool test."""
         self.assertEqual(True, True)
 
     def test_no_self_subscription(self):
-        "Модель Subscription не позволяет пользователю подписаться на самого себя."
+        """Модель Subscription не позволяет пользователю подписаться на самого
+        себя."""
         user = User.objects.create()
         constraint_name = "prevent_self_subscription"
         with self.assertRaisesMessage(IntegrityError, constraint_name):
             Subscription.objects.create(user=user, author=user)
 
     def test_unique_subscription(self):
-        "Нельзя повторно подписаться на пользователя."
-        user = User.objects.create(username="user")
-        author = User.objects.create(username="author")
-        Subscription.objects.create(user=user, author=author)
-        error = "UNIQUE constraint failed: users_subscription.user_id, users_subscription.author_id"
+        """Нельзя повторно подписаться на пользователя."""
+        Subscription.objects.create(
+            user=self.user,
+            author=self.author,
+        )
+        error = "UNIQUE constraint failed: users_subscription.user_id, "
+        error += "users_subscription.author_id"
         with self.assertRaisesMessage(IntegrityError, error):
-            Subscription.objects.create(user=user, author=author)
+            Subscription.objects.create(
+                user=self.user,
+                author=self.author,
+            )
